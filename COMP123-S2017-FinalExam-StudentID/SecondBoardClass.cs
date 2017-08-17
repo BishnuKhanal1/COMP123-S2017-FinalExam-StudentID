75c05a3111e79b5e6cee3aeacc6bdb667e4d3d0f
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-08-17 13:17:45 -0400

    SecondBoardClass Created

---
+++ COMP123-S2017-FinalExam-StudentID/SecondBoardClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Name: Bishnu Khanal
+ * Date: July 13, 2017
+ * Description: This is the SecondBoardClas
+ * Version:0.2 - Created SecondBoardClass
+ */
+namespace COMP123_S2017_FinalExam_StudentID
+{
+    /// <summary>
+    /// This is the SecondBoardClass
+    /// </summary>
+    class SecondBoardClass
+    {
+    }
+}
